8437fbf8b5c18877da81fa07d499b533b84b265f
minggas <fasfora@gmail.com>
2018-07-14 23:42:34 -0300

    fix decimal dot bug

--- js/main.js
+++ js/main.js
@@ -1,53 +1,84 @@
 /* jshint esversion:6 */
-let exp = [];
-let opInUse = true;
-let result;
-const numBtns = document.querySelectorAll('.pad [data-type="num"]').forEach(btn => btn.addEventListener('click', insertNum));
-const operationBtns = document.querySelectorAll('.pad [data-type="op"]').forEach(btn => btn.addEventListener('click', insertOp));
-const clear = document.querySelectorAll('.pad .clear').forEach(btn => btn.addEventListener('click', clearExp));
-const equal = document.getElementById('equal').addEventListener('click', evalute);
-
-function insertNum() {
-    if(result){
-        result = 0;
+const state = {
+    exp: [],
+    opInUse: true,
+    decimal: false,
+    result: null
+  }
+  const numBtns = document
+    .querySelectorAll('.pad [data-type="num"]')
+    .forEach(btn => btn.addEventListener("click", insertNum));
+  const operationBtns = document
+    .querySelectorAll('.pad [data-type="op"]')
+    .forEach(btn => btn.addEventListener("click", insertOp));
+  const clear = document
+    .querySelectorAll(".pad .clear")
+    .forEach(btn => btn.addEventListener("click", clearExp));
+  const equal = document
+    .getElementById("equal")
+    .addEventListener("click", evalute);
+  const visor = document.querySelector(".visor");
+  
+  function display() {
+    visor.innerHTML = `${state.exp.join("")}`;
+  }
+  
+  function insertNum() {
+    if (state.exp.length > 11) {
+        return;
     }
-    exp.push(this.innerHTML);
-    document.querySelector('.visor').innerHTML = `${exp.join('')}`;
-    opInUse = false;
-}
-
-function insertOp(){
-    if(result){
-        exp.push(result);
-        document.querySelector('.visor').innerHTML = `${exp.join('')}`;
+    if (state.result) {
+      state.result = 0;
+    }  
+    if (this.innerHTML === '0' && (state.exp[0] === '0' || !state.exp[0]) || (this.innerHTML === '.' && state.decimal)) {
+      return;
     }
-    if(!opInUse){
-        exp.push(this.innerHTML);
-        document.querySelector('.visor').innerHTML = `${exp.join('')}`;
+    if(this.innerHTML === '.' && !state.exp[0]) {
+      state.exp[0] = '0';
     }
-    opInUse = true;
-}
-
-function evalute() {
-    if(/[0-9]/.test(exp[exp.length - 1])){        
-        result = eval(exp.join(''));
-        document.querySelector('.visor').innerHTML = result;
-        exp = [];        
-    }        
-}
-
-function clearExp(){
-    if(this.dataset.type === 'exp'){
-        while(/[0-9]/.test(exp[exp.length - 1])){
-            exp.pop();
-        }
-        document.querySelector('.visor').innerHTML = `${exp.join('')}`;
-    }else if(this.dataset.type === 'all'){
-        exp = [];
-        result = null;
-        opInUse = true;
-        document.querySelector('.visor').innerHTML = '0';
+    if (this.innerHTML === '.' && !state.decimal){
+      state.decimal = true;
     }
-    
-    
-}
+    state.exp.push(this.innerHTML);
+    display();
+    state.opInUse = false;  
+  }
+  
+  function insertOp() {
+    if (state.result) {
+      state.exp.push(state.result);
+      display();
+    }
+    if (state.opInUse) {
+      state.exp.pop();
+    }
+    state.exp.push(this.innerHTML);
+    display();
+    state.opInUse = true;
+    state.decimal = false;
+  }
+  
+  function evalute() {
+    if (/[0-9]/.test(state.exp[state.exp.length - 1])) {
+      state.result = (eval(state.exp.join("")));
+      document.querySelector(".visor").innerHTML = state.result;
+      state.exp = [];
+      state.decimal = false;
+    }
+  }
+  
+  function clearExp() {
+    if (this.dataset.type === "exp") {
+      while (/[0-9]/.test(state.exp[state.exp.length - 1])) {
+        state.exp.pop();
+      }
+      display();
+    } else if (this.dataset.type === "all") {
+      state.exp = [];
+      state.result = null;
+      state.opInUse = true;
+      state.decimal = false;
+      visor.innerHTML = "0";
+    }
+  }
+  
